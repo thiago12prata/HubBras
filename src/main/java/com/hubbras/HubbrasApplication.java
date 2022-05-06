1a70464d74d3ebfcdc8b234f2b7e163ef5682aa2
Thiago PRata <thiago12prata@gmail.com>
2022-05-06 09:24:09 -0300

    Projeto criado

---
+++ src/main/java/com/hubbras/HubbrasApplication.java
@@ -0,0 +1,13 @@
+package com.hubbras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubbrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubbrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hubbras/HubbrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubbras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubbrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
